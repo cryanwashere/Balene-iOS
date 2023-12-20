9eef2b6a01ac83edfbf52e4c2fef0fbc9b9b488f
Cam <cjryanwashere@gmail.com>
2023-12-19 20:11:05 -0500

    Initial Commit

---
+++ Balene/BaleneApp.swift
@@ -0,0 +1,17 @@
+//
+//  BaleneApp.swift
+//  Balene
+//
+//  Created by CJ Ryan on 12/19/23.
+//
+
+import SwiftUI
+
+@main
+struct BaleneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
